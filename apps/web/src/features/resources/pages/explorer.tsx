7c0a5c0479f2017e3d236fe5aec9a8775c1ee78b
Kevin Mulier <kevin.mulier@outlook.fr>
2024-04-05 15:00:02 +0200

    feat: add dropdown menu on small screens

--- apps/web/src/features/resources/pages/explorer.tsx
+++ apps/web/src/features/resources/pages/explorer.tsx
@@ -44,7 +44,7 @@ export const Resources = () => {
                   <CategoryIcon
                     src={resourceCategory.image}
                     variant="resources"
-                    imgClassName="max-md:filter-white max-md:group-hover:filter-newOrange1 max-md:group-focus:filter-newOrange1"
+                    imgClassName="max-md:filter-white max-md:group-hover:filter-newOrange1"
                   />
                   <h3
                     className={cn(
