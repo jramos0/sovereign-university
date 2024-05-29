048c7cb1a5e9f97eb17aa238dcfa862dc1593ac5
jramos0 <jonathan007rm@gmail.com>
2024-05-29 12:11:42 -0600

    Comunnity Page

--- apps/web/src/features/resources/pages/builder-details.tsx
+++ apps/web/src/features/resources/pages/builder-details.tsx
@@ -5,7 +5,7 @@ import {
 import { Link, useParams } from '@tanstack/react-router';
 import { useEffect, useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { BsGithub, BsLink, BsTwitter } from 'react-icons/bs';
+import { BsGithub, BsBrowserChrome, BsTwitter } from 'react-icons/bs';
 import { GiBirdMask } from 'react-icons/gi';
 
 import { Card } from '../../../atoms/Card/index.tsx';
@@ -82,69 +82,74 @@ export const Builder = () => {
       backToCategoryButton
     >
       {builder && (
-        <Card className="mx-2 md:mx-auto">
-          <div className="my-4 w-full grid-cols-1 grid-rows-6 px-4 sm:grid-cols-3 sm:px-8 md:grid">
-            <h3 className="col-span-1 row-span-1 mb-4 text-3xl font-semibold uppercase text-blue-900 sm:text-4xl md:mb-8">
-              {builder?.name}
-            </h3>
-            <div className="col-span-2 row-span-1 mb-5 mt-1 font-light md:mb-0 md:ml-12">
-              {builder?.tags?.map((tag) => (
-                <Link to={'/resources/builders'} key={tag}>
-                  <Tag className="ml-1" size={isScreenMd ? 'm' : 's'}>
-                    {tag}
-                  </Tag>
-                </Link>
-              ))}
-            </div>
-            <div className="row-span-5 mb-4 flex flex-row flex-wrap items-center border-b-4 border-solid border-blue-900 md:mb-0 md:flex-col md:border-b-0 md:border-r-4 md:pb-10 md:pr-16">
-              <img
-                src={builder?.logo}
-                className="w-full"
-                alt={t('imagesAlt.sthRepresentingCompany')}
-              />
-              <div className="mx-2 my-6 flex w-full justify-evenly">
-                {builder?.githubUrl && (
-                  <a
-                    href={builder?.githubUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <BsGithub size={isScreenMd ? 32 : 24} />
-                  </a>
-                )}
-                {builder?.twitterUrl && (
-                  <a
-                    href={builder?.twitterUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <BsTwitter size={isScreenMd ? 32 : 24} />
-                  </a>
-                )}
-                {builder?.nostr && (
-                  <a
-                    href={builder?.nostr}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <GiBirdMask size={isScreenMd ? 32 : 24} />
-                  </a>
-                )}
-                {builder?.websiteUrl && (
-                  <a
-                    href={builder?.websiteUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <BsLink size={isScreenMd ? 32 : 24} />
-                  </a>
-                )}
+        <Card className="mx-2 bg-orange-600/30 border-2 border-orange-500 md:mx-auto">
+          <div className="my-4 w-full grid-cols-1  px-4  sm:px-8 md:grid">
+            <div className="mx-2 flex flex-col sm:flex-row w-full">
+              <div className=" mb-4  items-center  md:mb-0 md:flex-col  md:pb-10 md:pr-16">
+                <img
+                  src={builder?.logo}
+                  className="w-full"
+                  alt={t('imagesAlt.sthRepresentingCompany')}
+                />
+              </div>
+              <div className="col-span-2 row-span-1 mb-5 mt-1 font-light md:mb-0 md:ml-12">
+                <h3 className="col-span-1 row-span-1 mb-4 text-3xl font-semibold uppercase text-gray-200 sm:text-4xl md:mb-8">
+                  {builder?.name}
+                </h3>
+                {/* This Logos Divs*/}
+                <div className="mx-2 text-gray-200 my-6 flex w-auto space-x-4 justify-start">
+                  {builder?.githubUrl && (
+                    <a
+                      href={builder?.githubUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <BsGithub size={isScreenMd ? 32 : 24} />
+                    </a>
+                  )}
+                  {builder?.twitterUrl && (
+                    <a
+                      href={builder?.twitterUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <BsTwitter size={isScreenMd ? 32 : 24} />
+                    </a>
+                  )}
+                  {builder?.nostr && (
+                    <a
+                      href={builder?.nostr}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <GiBirdMask size={isScreenMd ? 32 : 24} />
+                    </a>
+                  )}
+                  {builder?.websiteUrl && (
+                    <a
+                      href={builder?.websiteUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <BsBrowserChrome size={isScreenMd ? 32 : 24} />
+                    </a>
+                  )}
+                </div>
+                {builder?.tags?.map((tag) => (
+                  <Link to={'/resources/builders'} key={tag}>
+                    <Tag
+                      className="ml-1 bg-gray-400/80 text-gray-100 text-sm rounded-sm"
+                      size={isScreenMd ? 'm' : 's'}
+                    >
+                      {tag}
+                    </Tag>
+                  </Link>
+                ))}
               </div>
             </div>
-
-            <div className="col-span-2 row-span-5 ml-0 flex flex-col space-y-4 text-sm md:ml-12">
+            <div className="col-span-2 sm:mt-12 row-span-5 ml-0 flex flex-col space-y-4 text-sm md:mx-12">
               <p
-                className="text-justify text-sm md:text-base"
+                className="text-justify text-gray-200 text-sm md:text-base"
                 style={{ whiteSpace: 'pre-line' }}
               >
                 {builder?.description}
